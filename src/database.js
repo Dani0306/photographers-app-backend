765128c06b340f111eff55792fcbedf425adeaea
Daniel Colorado <danicolorado0306@gmail.com>
2024-11-14 19:22:23 -0500

    last commit

--- src/database.js
+++ src/database.js
@@ -1,3 +1,5 @@
 import mongoose from "mongoose"
 
-export default () => mongoose.connect("mongodb://localhost:27017/photographers-app")
+export default () => mongoose.connect("mongodb+srv://Dani1243:Dani1243@padlet-clone.fwltftz.mongodb.net/?retryWrites=true&w=majority&appName=Padlet-Clone")
+
+// mongodb://localhost:27017/photographers-app
